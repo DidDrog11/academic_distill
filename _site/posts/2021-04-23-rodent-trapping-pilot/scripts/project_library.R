4d17eb528b1f8ce7ae353a81f6eba61467e36025
DidDrog11 <dsimons1987@gmail.com>
2021-04-23 17:05:46 +0100

    updating for conference

---
+++ _site/posts/2021-04-23-rodent-trapping-pilot/scripts/project_library.R
@@ -0,0 +1,38 @@
+if (!require("pacman")) install.packages("pacman")
+pkgs =
+  c("here",
+    "rgeos",
+    "ggspatial",
+    "rgdal",
+    "grid",
+    "tidyverse",
+    "magrittr",
+    "bib2df",
+    "googledrive",
+    "readxl",
+    "countrycode",
+    "ggimage",
+    "sf",
+    "OpenStreetMap",
+    "tmap",
+    "maptools",
+    "raster",
+    "ggmap",
+    "taxize",
+    "distill",
+    "magrittr",
+    "rvest",
+    "tmap",
+    "stars",
+    "leaflet",
+    "leaflet.opacity",
+    "vegan",
+    "knitr",
+    "waffle",
+    "cowplot",
+    "iNEXT"
+  )
+pacman::p_load(pkgs, character.only = T)
+
+lalehun_coords <- c(-11.0803, 8.197533)
+seilama_coords <- c(-11.193628469657279, 8.122285428353395)
---
+++ _site/posts/2021-04-23-rodent-trapping-pilot/scripts/rodent_data_management.R
@@ -0,0 +1,61 @@
+source(here::here("_posts", "2021-04-23-rodent-trapping-pilot", "scripts", "project_library.R"))
+
+## For pilot study only
+
+trapped_rodents <- readxl::read_xlsx(path = here("_posts", "2021-04-23-rodent-trapping-pilot", "data", "trap_sites_all.xlsx"), sheet = 3) %>%
+  mutate(date = as.Date(date, format = "%Y- %m-%d")) %>%
+  filter(date <=	"2020-12-09")
+habitat_names <- c("Forest/fallow land", "Distal agriculture", "Proximal agriculture", "Village")
+names(habitat_names) = c("forest/fallow", "distal_agriculture", "proximal_agriculture", "village")
+
+trap_sites <- read_csv(here("_posts", "2021-04-23-rodent-trapping-pilot", "data", "trap_sites.csv")) %>%
+  mutate(habitat = recode(habitat,
+         "developing_banana_plantation" = "proximal_agriculture",
+         "palm_plantation" = "proximal_agriculture",
+         "cassava_plantation" = "distal_agriculture",
+         "cacao_coffee_plantation" = "distal_agriculture",
+         "harvested_dry_rice_farm" = "distal_agriculture",
+         "disturbed_forest, fallow_5y" = "forest/fallow",
+         "forest_fallow" = "forest/fallow",
+         "village_periphery" = "village"),
+         habitat = recode_factor(habitat, !!!habitat_names),
+         village = case_when(village == "lalehun" ~ "Lalehun",
+                             village == "seilama" ~ "Seilama")) %>%
+  group_by(habitat) %>%
+  filter(visit == 1)
+
+location_rodents <- trapped_rodents %>%
+  dplyr::select(rodent_id, trap_night, trap_id, initial_species_id) %>%
+  left_join(., trap_sites, 
+            by = c("rodent_id")) %>%
+  mutate(trap_night = ifelse(trap_night.x == trap_night.y, trap_night.x, "WARNING")) %>%
+  dplyr::select(rodent_id, trap_night, initial_species_id, village, habitat)
+
+trapped_rodents %<>%
+  mutate(initial_species_id = as_factor(initial_species_id))
+
+genus_allocation <- c("l_sikapusi" = "lophuromys", 
+                      "crocidura_spp" = "crocidura",
+                      "l_striatus" = "lemniscomys",
+                      "m_minutoides" = "mus",
+                      "malacomys_edwardsi" = "malacomys",
+                      "praomys_spp" = "praomys",
+                      "gerbillinae_spp" = "gerbillinae",
+                      "mastomys_spp" = "mastomys",
+                      "hylomyscus_spp" = "hylomyscus",
+                      "hybomys_spp" = "hybomys",
+                      "r_rattus" = "rattus")
+family_allocation <- c("lophuromys" = "muridae",
+                       "crocidura" = "soricidae",
+                       "lemniscomys" = "muridae",
+                       "mus" = "muridae",
+                       "malacomys" = "muridae",
+                       "praomys" = "muridae",
+                       "gerbillinae" = "muridae",
+                       "mastomys" = "muridae",
+                       "hylomyscus" = "muridae",
+                       "hybomys" = "muridae",
+                       "rattus" = "muridae")
+trapped_rodents %<>%
+  mutate(genus = recode(initial_species_id, !!!genus_allocation),
+         family = recode(genus, !!!family_allocation))
